c2e62b419b5005a1f042562359ed5c58eba17cf4
Luis Esteban <ebaldasseroni@gmail.com>
2022-12-15 11:27:27 -0300

    path parameters with classes

---
+++ practice/API_00_PATH/path_parameters/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+class ModelName(str, Enum):
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
+
+@app.get('/models/{model_name}')
+async def get_model(model_name : ModelName):
+    if model_name is ModelName.alexnet:
+        return {'Model name': model_name, 'Message': 'Deep Learning FTW!' }
+
+    if model_name.value == 'lenet':
+        return {'Model name': model_name, 'Message': 'LeCNN all the images'}
+
+    return {'Model name': model_name, 'Message': 'Have some residuals'}
