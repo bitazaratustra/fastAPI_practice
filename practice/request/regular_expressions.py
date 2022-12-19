e1087a8ae741a30e47ca9aac329198c54bdd0444
Luis Esteban <ebaldasseroni@gmail.com>
2022-12-19 19:52:02 -0300

    Trying on query parameters and string validation

--- practice/request/regular_expressions.py
+++ practice/request/regular_expressions.py
@@ -25,9 +25,52 @@ async def read_items(
 
 
 
+@app.get("/items/exclude_from_API")
+async def read_items(
+    hidden_query: Union[str, None] = Query(default=None, include_in_schema=False)
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    else:
+        return {"hidden_query": "Not found"}
 
-@app.get("/items/items")
+
+
+
+@app.get("/items/default_value")
 async def read_items(q: str = Query(default="fixquery", min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items/required_value")
+async def read_items(q: str = Query(min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items/metadata")
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+
+@app.get("/items/alias_parameter")
+async def read_items(q: Union[str, None] = Query(default=None, alias="item-query")):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
