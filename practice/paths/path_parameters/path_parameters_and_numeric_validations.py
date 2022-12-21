32d5937a656822e29e2e7378e325ad057ede25a2
Luis Esteban <ebaldasseroni@gmail.com>
2022-12-21 19:48:01 -0300

    oath, queries and body parameters using Path() from fastAPI

--- practice/paths/path_parameters/path_parameters_and_numeric_validations.py
+++ practice/paths/path_parameters/path_parameters_and_numeric_validations.py
@@ -7,13 +7,18 @@ async def read_file(file_path: str):
     return {'File Path': file_path}
 
 
-from fastapi import FastAPI, Path
-
-app = FastAPI()
-
 
 @app.get("/items/{item_id}")
 async def read_items(*, item_id: int = Path(default=..., title="The ID of the item to get"), q: str):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+
+@app.get("/items/{item_id}")
+async def read_items(*, item_id: int = Path(default=...,title="The ID of the item to get"), q: str):
     results = {"item_id": item_id}
     if q:
         results.update({"q": q})
