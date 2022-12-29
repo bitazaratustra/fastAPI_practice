09b837f82cc4fbfbd313408a04d861bc68ae44c4
Luis Esteban <ebaldasseroni@gmail.com>
2022-12-29 12:07:25 -0300

    fields

---
+++ practice/fields/field.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Item (BaseModel):
+    name : str
+    description : Union[str, None] = Field (
+        default = None, title="The description of the item", max_length=300
+    )
+    price : float = Field(gt=0, description="The price must be grater than zero")
+    tax : Union [float, None] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(embded=True, default=None)):
+    results = {'item_id': item_id, 'item': item}
+    return results
