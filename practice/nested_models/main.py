ed980886ecfd9151b0ac528a2cbd475adefff4db
Luis Esteban <ebaldasseroni@gmail.com>
2022-12-30 13:47:12 -0300

    Use Nested Models with Pydantic

--- practice/nested_models/main.py
+++ practice/nested_models/main.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Union, Set
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -14,7 +14,23 @@ class Item(BaseModel):
     tags: list = []
 
 
+# Use "set" to create an unique class element item
+
+class Item2(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: Set[str] = set()
+
+
 @app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item2):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+@app.put("/items/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
