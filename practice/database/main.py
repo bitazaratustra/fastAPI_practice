7de72f2936a44075725e42040692943cf7e9c738
Luis Esteban <ebaldasseroni@gmail.com>
2023-01-07 16:15:14 -0300

    databse connection

---
+++ practice/database/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from . import models
+from . import schemas
+from sqlalchemy import create_engine
+
+app = FastAPI()
+SQLALCHAMY_DATABASE_URL = 'sqlite:///database.db:'
+
+engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+models.Base.metadata.create_all(engine)
+
+@app.post('/blog')
+async def create (request: schemas.Blog):
+    return request
